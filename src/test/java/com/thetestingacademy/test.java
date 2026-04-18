bca2eafa693caf6587fc9fb97215fba476654cd9
apple <tb1169cg12@gmail.com>
2026-04-18 16:36:19 +0530

    first file

--- src/test/java/com/thetestingacademy/test.java
+++ src/test/java/com/thetestingacademy/test.java
@@ -1,5 +1,12 @@
 package com.thetestingacademy;
 
+import org.apache.log.output.net.SocketOutputTarget;
+import org.testng.annotations.Test;
+
 public class test {
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
 
 }
